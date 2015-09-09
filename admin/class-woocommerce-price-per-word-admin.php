49334853041dd30d9fa70681d129b62545f8f362
kcppdevelopers <kcppdevelopers@gmail.com>
2015-09-09 12:12:51 +0530

    init commit, ref #1

---
+++ admin/class-woocommerce-price-per-word-admin.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @package    Woocommerce_Price_Per_Word
+ * @subpackage Woocommerce_Price_Per_Word/admin
+ * @author     Angell EYE <service@angelleye.com>
+ */
+class Woocommerce_Price_Per_Word_Admin {
+
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $plugin_name    The ID of this plugin.
+     */
+    private $plugin_name;
+
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $version    The current version of this plugin.
+     */
+    private $version;
+
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @param      string    $plugin_name       The name of this plugin.
+     * @param      string    $version    The version of this plugin.
+     */
+    public function __construct($plugin_name, $version) {
+
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+    }
+
+    /**
+     * Register the stylesheets for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_styles() {
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/woocommerce-price-per-word-admin.css', array(), $this->version, 'all');
+    }
+
+    /**
+     * Register the JavaScript for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts() {
+        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/woocommerce-price-per-word-admin.js', array('jquery'), $this->version, false);
+    }
+
+}
---
+++ woocommerce-price-per-word.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       WooCommerce Price Per Word
+ * Plugin URI:        https://www.angelleye.com/
+ * Description:       Sell products using a "price per word" method based on a document the customer uploads.
+ * Version:           1.0.0
+ * Author:            Angell EYE
+ * Author URI:        http://www.angelleye.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       woocommerce-price-per-word
+ * Domain Path:       /languages
+ */
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-woocommerce-price-per-word-activator.php
+ */
+function activate_woocommerce_price_per_word() {
+    require_once plugin_dir_path(__FILE__) . 'includes/class-woocommerce-price-per-word-activator.php';
+    Woocommerce_Price_Per_Word_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-woocommerce-price-per-word-deactivator.php
+ */
+function deactivate_woocommerce_price_per_word() {
+    require_once plugin_dir_path(__FILE__) . 'includes/class-woocommerce-price-per-word-deactivator.php';
+    Woocommerce_Price_Per_Word_Deactivator::deactivate();
+}
+
+register_activation_hook(__FILE__, 'activate_woocommerce_price_per_word');
+register_deactivation_hook(__FILE__, 'deactivate_woocommerce_price_per_word');
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path(__FILE__) . 'includes/class-woocommerce-price-per-word.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_woocommerce_price_per_word() {
+
+    $plugin = new Woocommerce_Price_Per_Word();
+    $plugin->run();
+}
+
+run_woocommerce_price_per_word();

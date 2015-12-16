91fe1f13545086cf2093d9168c2349f930f7a451
kcppdevelopers <kcppdevelopers@gmail.com>
2015-12-16 18:50:07 +0530

    add dynamic value, ref #18

--- admin/js/woocommerce-price-per-word-admin.js
+++ admin/js/woocommerce-price-per-word-admin.js
@@ -9,6 +9,7 @@ jQuery(function ($) {
         } else {
             var wppw_product_type = 'Word ';
         }
+        
         $('#_price_per_word').change(function () {
             if ($(this).is(':checked')) {
                 $("label[for='_regular_price']").text('Price Per ' + wppw_product_type + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
@@ -18,30 +19,47 @@ jQuery(function ($) {
                 $(".variable_pricing p.form-row-first label").text('Regular Price ' + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
             }
         });
-        $('.variations_tab a').click(function () {
+        $('.variations_tab a, .woocommerce_variation').click(function () {
             if ($('#_price_per_word').is(':checked')) {
                 setTimeout(function () {
                     $(".variable_pricing p.form-row-first label").text('Price Per '+ wppw_product_type + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
-                }, 4000);
+                }, 5000);
 
             } else {
                 setTimeout(function () {
-                    $(".variable_pricing p.form-row-first label").text('Regular Price' + wppw_product_type + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
-                }, 4000);
+                    $(".variable_pricing p.form-row-first label").text('Regular Price ' + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
+                }, 5000);
             }
         });
-        $("#product-type").change(function () {
+        
+        $(document).on('change', '#variable_product_options', function () {
             if ($('#_price_per_word').is(':checked')) {
                 setTimeout(function () {
                     $("label[for='_regular_price']").text('Price Per '+ wppw_product_type + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
                     $(".variable_pricing p.form-row-first label").text('Price Per ' + wppw_product_type + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
-                }, 4000);
+                }, 10000);
 
             } else {
                 setTimeout(function () {
                     $("label[for='_regular_price']").text('Regular Price ' + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
                     $(".variable_pricing p.form-row-first label").text('Regular Price ' + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
-                }, 4000);
+                }, 10000);
+            }
+        });
+        
+       
+        $("#product-type, #variable_product_options .woocommerce_variations").change(function () {
+            if ($('#_price_per_word').is(':checked')) {
+                setTimeout(function () {
+                    $("label[for='_regular_price']").text('Price Per '+ wppw_product_type + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
+                    $(".variable_pricing p.form-row-first label").text('Price Per ' + wppw_product_type + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
+                }, 5000);
+
+            } else {
+                setTimeout(function () {
+                    $("label[for='_regular_price']").text('Regular Price ' + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
+                    $(".variable_pricing p.form-row-first label").text('Regular Price ' + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
+                }, 5000);
             }
         });
 
@@ -49,13 +67,13 @@ jQuery(function ($) {
             setTimeout(function () {
                 $("label[for='_regular_price']").text('Price Per ' + wppw_product_type + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
                 $(".variable_pricing p.form-row-first label").text('Price Per ' + wppw_product_type + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
-            }, 4000);
+            }, 5000);
 
         } else {
             setTimeout(function () {
                 $("label[for='_regular_price']").text('Regular Price ' + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
                 $(".variable_pricing p.form-row-first label").text('Regular Price ' + woocommerce_price_per_word_params.woocommerce_currency_symbol_js);
-            }, 4000);
+            }, 5000);
         }
     });
 });
